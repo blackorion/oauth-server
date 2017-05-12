bcf9dd4f8c7e7981ffad18666ae93981d36281e5
Sergey Ivanov <sergey.ivanov@sedenis.com>
2017-05-12 13:15:12 +0300

    simplest account verification

--- src/main/java/com/sidenis/AccountUserDetailsService.java
+++ src/main/java/com/sidenis/AccountUserDetailsService.java
@@ -1,19 +1,29 @@
 package com.sidenis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-class AccountUserDetailsService implements UserDetailsService {
+@Service
+public class AccountUserDetailsService implements UserDetailsService {
+
+    @Value("${test.security.user.password}")
+    private String userPassword = "benspassword";
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if ("trusted-user".equals(username))
-            return new User(username, "test123", true, true, true, true, AuthorityUtils.createAuthorityList("USER", "ROLE_ADMIN"));
+        if ("ben".equals(username))
+            return createUser(username);
         else
             throw new UsernameNotFoundException("no user found with name: " + username);
+    }
+
+    private User createUser(String username) {
+        return new User(username, userPassword, true, true, true, true, AuthorityUtils.createAuthorityList("ROLE_USER", "ROLE_ADMIN"));
     }
 
 }
