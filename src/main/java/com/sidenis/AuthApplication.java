3fea3cb5a9d04062c54debfc5a7801f4c761c633
Sergey Ivanov <sergey.ivanov@sedenis.com>
2017-05-12 13:43:55 +0300

    ldap file authorization mocked in file

--- src/main/java/com/sidenis/AuthApplication.java
+++ src/main/java/com/sidenis/AuthApplication.java
@@ -1,7 +1,11 @@
 package com.sidenis;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.authentication.encoding.LdapShaPasswordEncoder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 
 @SpringBootApplication
 public class AuthApplication {
@@ -10,4 +14,14 @@ public class AuthApplication {
         SpringApplication.run(AuthApplication.class, args);
     }
 
+    @Autowired
+    public void init(AuthenticationManagerBuilder builder, DefaultSpringSecurityContextSource contextSource) throws Exception {
+        builder.ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource(contextSource)
+                .passwordCompare()
+                .passwordEncoder(new LdapShaPasswordEncoder())
+                .passwordAttribute("userPassword");
+    }
 }
---
+++ src/main/java/com/sidenis/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.sidenis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+
+import java.util.Collections;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
+
+    @Bean
+    public DefaultSpringSecurityContextSource contextSource() {
+        return new DefaultSpringSecurityContextSource(Collections.singletonList("ldap://localhost:9321/"), "dc=sidenis,dc=com");
+    }
+
+}
